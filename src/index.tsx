702b3335499930ecf3de71237a231dc81d6c7a00
Daniel <danielkondlatsch.p@gmail.com>
2022-07-11 22:52:21 -0300

    Separamento das telas completas e adição da imagem de fundo e da logo da Compass Uol

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./reset.css";
 import "./index.css";
+import Login from "./pages/Login";
+import RightScreen from "./pages/RightScreen";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <main>
-      <h3>teste</h3>
-    </main>
+    <body>
+      <Login />
+      <RightScreen />
+    </body>
   </React.StrictMode>
 );
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./Login.module.scss";
+import { ReactComponent as Logo } from "assets/logo-compass.svg";
+
+export default function Login() {
+  return (
+    <main>
+      <div className={styles.content}>
+        <h3 className={styles.h3}>Olá,</h3>
+      </div>
+    </main>
+  );
+}
