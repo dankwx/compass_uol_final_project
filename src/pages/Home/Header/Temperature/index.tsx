d3e86be6fb934d8e6508552c323f218a9d6c01ca
Daniel <danielkondlatsch.p@gmail.com>
2022-07-12 21:14:03 -0300

    API OpenWeather usada para fazer a mostragem da temperatura dinamica, concluido com sucesso

--- src/pages/Home/Header/Temperature/index.tsx
+++ src/pages/Home/Header/Temperature/index.tsx
@@ -1,14 +1,44 @@
 import styles from "./Temperature.module.scss";
 import { ReactComponent as Cloud } from "./cloud-ico.svg";
+import { useEffect, useState } from "react";
 
 export default function Temperature() {
+  const [temperature, setTemperature] = useState<number>(0);
+  const [location, setLocation] = useState<string>("");
+
+  //get the temperature from the API and convert it to Celsius
+  useEffect(() => {
+    const getTemperature = async () => {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=Mafra,br&appid=1b80fdb1720dd149b56848e6c48fb8d2"
+      );
+      const data = await response.json();
+      const temperature = Math.round(data.main.temp - 273.15);
+      setTemperature(temperature);
+    };
+    getTemperature();
+  }, []);
+
+  //get the location from the API
+  useEffect(() => {
+    const getLocation = async () => {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=Mafra,br&appid=1b80fdb1720dd149b56848e6c48fb8d2"
+      );
+      const data = await response.json();
+      const location = data.name;
+      setLocation(location);
+    };
+    getLocation();
+  }, []);
+
   return (
     <div className={styles.content}>
-      <h3 className={styles.city}>Passo Fundo - RS</h3>
+      <h3 className={styles.city}>{location} - SC</h3>
 
       <div className={styles.temperature}>
         <Cloud width={60} height={35} />
-        <h2 className={styles.cityTemp}>22º</h2>
+        <h2 className={styles.cityTemp}>{temperature}º</h2>
       </div>
     </div>
   );
--- src/pages/Home/Header/Time/index.tsx
+++ src/pages/Home/Header/Time/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import styles from "./Time.module.scss";
 
 export default function Time() {
-  const [relogio, setRelogio] = useState<any | null>(null);
+  const [clock, setClock] = useState<any | null>(null);
   const [day, setDay] = useState<any | null>(null);
 
   useEffect(() => {
@@ -24,14 +24,17 @@ export default function Time() {
       const date = new Date();
       const hora = date.getHours();
       const minutos = date.getMinutes();
-      setRelogio(`${hora}:${minutos}`);
+      const clock = `${hora < 10 ? `0${hora}` : hora}:${
+        minutos < 10 ? `0${minutos}` : minutos
+      }`;
+      setClock(clock);
     }, 1000);
   }, []);
 
   return (
     <div className={styles.content}>
       <div className={styles.hourDays}>
-        <h1 className={styles.hours}>{relogio}</h1>
+        <h1 className={styles.hours}>{clock}</h1>
         <h2 className={styles.days}>{day}</h2>
       </div>
     </div>
