aa59c42af03fcc0b18d1df1a44c235a4351ca548
Daniel <danielkondlatsch.p@gmail.com>
2022-07-20 02:33:40 -0300

    Refatoração de códigos, remoção de console de erros

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './reset.css';
 import './index.css';
-import Router from './routes';
+import AppRouter from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <body>
-      <Router />
-    </body>
+    <AppRouter />
   </React.StrictMode>
 );
--- src/routes.js
+++ src/routes.js
@@ -2,33 +2,14 @@ import Home from 'pages/Home';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { auth } from 'firebase-config';
 
 export default function AppRouter() {
-  const [loggedInUser, setloggedInUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setloggedInUser(user.email);
-        console.log(loggedInUser);
-        return true;
-      } else {
-        setloggedInUser(null);
-        console.log('no user');
-        return false;
-      }
-    });
-    return () => unsubscribe();
-  }, []);
-
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/Home' element={<Home />} />
-        <Route path='/Register' element={<Register />} />
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/Home' element={<Home />} />
+        <Route exact path='/Register' element={<Register />} />
       </Routes>
     </Router>
   );
